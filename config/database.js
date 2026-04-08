b4e7554944fb83e82b9634879a63140e5aa7a90e
lvthinh1 <le.vietthinh2002@gmail.com>
2026-04-08 10:51:39 +0700

    Initial commit: Node.js Todo application

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    remoteUrl: 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl: process.env.MONGODB_URI || 'mongodb://localhost/meanstacktutorials'
+};
